986a6830afc4b83b1ae9253b652341f839ba563c
Camilo <camilobeltran174@gmail.com>
2024-12-07 10:55:32 -0500

    Add logic to AsignacionesPage

--- src/pages/Asignaciones/AsignacionesPage.jsx
+++ src/pages/Asignaciones/AsignacionesPage.jsx
@@ -1,8 +1,104 @@
+import { useState } from 'react';
+
+import VisualizePDF from '../../components/VisualizePDF/VisualizePDF';
+import AsignacionesPDF from './PDF/AsignacionesPDF';
+import NamesInputs from './components/NamesInputs';
+
+import { getProgrammableDays, daysByMonths, nextMonths } from './PDF/util/schedule';
+
 export default function AsignacionesPage() {
 
+    // Initialize Schedule
+    const [dateStart, setDateStart] = useState(null)
+    const [weekDay, setWeekDay] = useState(null)
+    const [weekendDay, setWeekendDay] = useState(null)
+
+    // Content
+    const [schedule, setSchedule] = useState([])
+    const [peopleScheduled, setPeopleScheduled] = useState([])
+
+    // PDF generated
+    const currentDocument = <AsignacionesPDF schedule={schedule} peopleScheduled={peopleScheduled} />
+
+    // Behaviors
+    const handleDateStart = (e) => {
+        setDateStart(e.target.value + '-1');
+    };
+    const handleSelectWeekDay = (e) => {
+        setWeekDay(e.target.value)
+    }
+    const handleSelectWeekendDay = (e) => {
+        setWeekendDay(e.target.value)
+    }
+    const handleBtnDateStart = (e) => {
+        e.preventDefault();
+
+        // Validate theres no empty field
+        if (dateStart != null && weekDay != null && weekendDay != null) {
+            const programmableDays = getProgrammableDays(dateStart, weekDay, weekendDay)
+
+            const daysOnMonths = daysByMonths(programmableDays)
+            const correspondingMonths = nextMonths(dateStart, daysByMonths(programmableDays).length)
+
+            const newSchedule = correspondingMonths.map((month, index) => ({
+                month: month,
+                day: daysOnMonths[index],
+            }));
+
+            setSchedule(newSchedule)
+
+        } else {
+            alert("Asegurate de llenar todos los campos primero")
+        }
+    }
+
     return (
-        <div>
-            <h1>Asignaciones</h1>
+        <div style={{
+            backgroundColor: '#0c4ba6',
+            color: 'white'
+        }}>
+            <div style={{ display: 'block', margin: 'auto', paddingTop: '40px', width: '65vw' }}>
+                <form>
+                    <h1 style={{ fontSize: '36px', width: 'fit-content', margin: 'auto auto 20px auto' }}>Asignaciones</h1>
+                    <div style={{ width: 'fit-content', fontSize: '1.6rem', margin: 'auto', marginBottom: '10px' }}>
+                        <label style={{ marginRight: '10px' }} htmlFor="date-start">Mes de incio </label>
+                        <input style={{ fontSize: '18px' }} type="month" id='date-start' onChange={handleDateStart} /> {/* Cambiar a type="month" */}
+                    </div>
+                    <div style={{ margin: 'auto auto 20px auto', width: 'fit-content' }}>
+                        <div style={{ width: 'fit-content', margin: 'auto auto 10px auto', fontSize: '1.6rem' }} >
+                            <label style={{ marginRight: '20px ' }} htmlFor="en-semana">Día entre semana</label>
+                            <select style={{ fontSize: '1.3rem' }} name="select" id='en-semana' onChange={handleSelectWeekDay}>
+                                <option value="">- Seleccionar -</option>
+                                <option value="1">Lunes</option>
+                                <option value="2">Martes</option>
+                                <option value="3">Miércoles</option>
+                                <option value="4">Jueves</option>
+                                <option value="5">Viernes</option>
+                            </select>
+                        </div>
+                        <div style={{ width: 'fit-content', margin: 'auto', fontSize: '1.6rem' }} >
+                            <label style={{ marginRight: '20px ' }} htmlFor="fin-semana">Día fin de semana</label>
+                            <select style={{ fontSize: '1.3rem' }} name="select" id="fin-semana" onChange={handleSelectWeekendDay} autoFocus>
+                                <option value="">- Seleccionar -</option>
+                                <option value="6">Sábado</option>
+                                <option value="0">Domingo</option>
+                            </select>
+                        </div>
+                    </div>
+                    <button style={{
+                        display: 'block',
+                        margin: 'auto',
+                        padding: '10px 20px',
+                        fontSize: '18px'
+                    }} onClick={handleBtnDateStart}>Generar Campos</button>
+
+                </form>
+
+            </div>
+
+            <NamesInputs schedule={schedule} setPeopleScheduled={setPeopleScheduled} />
+
+            <VisualizePDF Document={currentDocument} />
         </div>
     )
 }
