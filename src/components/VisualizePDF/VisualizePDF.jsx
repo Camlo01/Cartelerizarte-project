a0d26b0d706b6341322f618c03fd296e1d12fa22
Camilo <camilobeltran174@gmail.com>
2024-12-19 11:20:54 -0500

    Add download button with file name

--- src/components/VisualizePDF/VisualizePDF.jsx
+++ src/components/VisualizePDF/VisualizePDF.jsx
@@ -1,13 +1,20 @@
-import { PDFViewer} from "@react-pdf/renderer";
+import { PDFViewer, PDFDownloadLink } from "@react-pdf/renderer";
 
 import './VisualizePDF.css'
 
-export default function VisualizePDF({ Document: DocumentToDisplay }) {
+export default function VisualizePDF({ Document, fileName }) {
 
     return (
-        <div className="PDFViewerContainer">
+        <div className="PDFViewerContainer" style={{ display: 'flex', flexDirection: 'column' }}>
+            <PDFDownloadLink
+                style={{ maxWidth: '320px', margin: '20px auto', padding: " 10px 60px", backgroundColor: 'white', color: 'black' }}
+                document={Document} fileName={fileName || "file"}>
+                {({ blob, url, loading, error }) =>
+                    loading ? 'Cargando...' : 'Descargar'
+                }
+            </PDFDownloadLink>
             <PDFViewer className="PDFViewerElement">
-                {DocumentToDisplay}
+                {Document}
             </PDFViewer>
         </div>
     )
