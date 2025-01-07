f514259a6276db57cb0419f70360c40e3523b1a9
Camilo <camilobeltran174@gmail.com>
2025-01-07 15:58:23 -0500

    Add new elements in dropdown-menu

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,18 +1,32 @@
-
 import { Link } from "react-router-dom";
-
-import React from 'react';
-import './NavBar.css'
+import React, { useState } from "react";
+import './NavBar.css';
 
 function NavBar() {
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+    const toggleDropdown = () => {
+        setIsDropdownOpen(!isDropdownOpen);
+    };
 
     return (
         <nav>
             <ul>
-                <li><Link to={'/'}>Cartelera</Link></li>
-                <li><Link to={'/vida-y-ministerio'}>Vida y Ministerio</Link></li>
-                <li><Link to={'/asignaciones'}>Asignaciones</Link></li>
-                <li><Link to={'/atalaya'}>Atalaya</Link></li>
+                <li><Link to="/">Cartelera</Link></li>
+                <li
+                    onClick={toggleDropdown}
+                >
+                    <span className="dropdown-title">{"Cronogramas"}</span>
+                    {isDropdownOpen && (
+                        <ul className="dropdown-menu">
+                            <li><Link to="/vida-y-ministerio">Vida y Ministerio</Link></li>
+                            <li><Link to="/asignaciones">Asignaciones</Link></li>
+                            <li><Link to="/atalaya">Atalaya</Link></li>
+                            <li><Link to="/conferencias">Conferencias</Link></li>
+                            <li><Link to="/limpieza">Limpieza</Link></li>
+                        </ul>
+                    )}
+                </li>
             </ul>
         </nav>
     );
