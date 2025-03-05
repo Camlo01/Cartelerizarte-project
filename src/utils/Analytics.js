ccdf95c15d254088d3b865f4033546c57add6935
Camilo <camilobeltran174@gmail.com>
2025-03-05 13:21:30 -0500

    Refactor GA initialization to use environment variable (VITE_GA_ID)

--- src/utils/Analytics.js
+++ src/utils/Analytics.js
@@ -1,14 +1,17 @@
 
 export const initGA = () => {
+
+    const GA_ID = import.meta.env.VITE_GA_ID;
+
     const script = document.createElement('script');
     script.async = true;
-    script.src = `https://www.googletagmanager.com/gtag/js?id=G-1YQM325WM7`;
+    script.src = `https://www.googletagmanager.com/gtag/js?id=${GA_ID}`;
     document.head.appendChild(script)
 
     script.onload = () => {
         window.dataLayer = window.dataLayer || [];
         function gtag() { window.dataLayer.push(arguments) }
         gtag('js', new Date())
-        gtag('config', 'G-1YQM325WM7')
+        gtag('config', GA_ID)
     }
 }
